36146ce0dc1df3dc8dd4819aaeb6289a70827186
Andres Osorio <aosorios@psl.com.co>
2021-05-31 15:42:33 -0500

    Add social media ui

---
+++ flutter_social_media_ui/lib/screens/home_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_social_ui/data/data.dart';
+import 'package:flutter_social_ui/models/user_model.dart';
+import 'package:flutter_social_ui/widgets/custom_drawer.dart';
+import 'package:flutter_social_ui/widgets/following_users.dart';
+import 'package:flutter_social_ui/widgets/posts_carousel.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+    _pageController = PageController(initialPage: 0, viewportFraction: 0.8);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Theme.of(context).primaryColor),
+        title: Text(
+          'FRENZY',
+          style: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 10.0,
+          ),
+        ),
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorWeight: 3.0,
+          labelColor: Theme.of(context).primaryColor,
+          labelStyle: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w600,
+          ),
+          unselectedLabelStyle: TextStyle(fontSize: 18.0),
+          tabs: [
+            Tab(
+              text: 'Trending',
+            ),
+            Tab(
+              text: 'Latest',
+            ),
+          ],
+        ),
+      ),
+      drawer: CustomDrawer(),
+      body: ListView(
+        children: [
+          FollowingUsers(),
+          PostsCarousel(
+            pageController: _pageController,
+            title: 'Posts',
+            posts: posts,
+          ),
+        ],
+      ),
+    );
+  }
+}
