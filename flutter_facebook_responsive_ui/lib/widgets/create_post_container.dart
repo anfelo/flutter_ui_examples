2487db6cbb4b75e9c22ed9a3fb4b9b349781c5cb
Andres Osorio <aosorios@psl.com.co>
2021-06-01 15:31:03 -0500

    Add facebook responsive ui example

---
+++ flutter_facebook_responsive_ui/lib/widgets/create_post_container.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/models/models.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/widgets.dart';
+
+class CreatePostContainer extends StatelessWidget {
+  final User currentUser;
+
+  CreatePostContainer({Key key, @required this.currentUser}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 0.0),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              ProfileAvatar(imageUrl: currentUser.imageUrl),
+              const SizedBox(width: 8.0),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration.collapsed(
+                    hintText: 'What\'s on your mind?',
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const Divider(height: 10.0, thickness: 0.5),
+          Container(
+            height: 40.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.videocam,
+                    color: Colors.red,
+                  ),
+                  label: Text(
+                    'Live',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                const VerticalDivider(width: 8.0),
+                TextButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.photo_library,
+                    color: Colors.green,
+                  ),
+                  label: Text(
+                    'Photo',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                const VerticalDivider(width: 8.0),
+                TextButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.video_call,
+                    color: Colors.purpleAccent,
+                  ),
+                  label: Text(
+                    'Room',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ flutter_facebook_responsive_ui/lib/widgets/rooms.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/config/palette.dart';
+import 'package:flutter_facebook_responsive_ui/models/models.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/widgets.dart';
+
+class Rooms extends StatelessWidget {
+  final List<User> onlineUsers;
+
+  Rooms({Key key, @required this.onlineUsers}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60.0,
+      color: Colors.white,
+      child: ListView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 4.0),
+        scrollDirection: Axis.horizontal,
+        itemCount: 1 + onlineUsers.length,
+        itemBuilder: (BuildContext context, int index) {
+          if (index == 0) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: _CreateRoomButton(),
+            );
+          }
+          final User user = onlineUsers[index - 1];
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: ProfileAvatar(
+              imageUrl: user.imageUrl,
+              isActive: true,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _CreateRoomButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      onPressed: () {},
+      child: Row(
+        children: [
+          ShaderMask(
+            shaderCallback: (rect) =>
+                Palette.createRoomGradient.createShader((rect)),
+            child: Icon(
+              Icons.video_call,
+              size: 35.0,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(width: 4.0),
+          Text('Create\nRoom'),
+        ],
+      ),
+      style: OutlinedButton.styleFrom(
+        primary: Palette.facebookBlue,
+        side: BorderSide(
+          width: 3.0,
+          color: Colors.blueAccent[100],
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+      ),
+    );
+  }
+}
